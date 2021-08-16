3da2a541d41ac9e32a8525332e4d45fdf7f7b830
Artem <artkola122@gmail.com>
2021-08-16 13:44:09 +0300

    HW5 complete

--- index.js
+++ index.js
@@ -24,8 +24,64 @@ function arrayFunc(elem) {
 }
 let getUpdatedArr = arrayFunc();
 console.log(getUpdatedArr(101));
-console.log(getUpdatedArr(202));
-console.log(getUpdatedArr({name: 'Vasya'}));
-console.log(getUpdatedArr());
-console.log(getUpdatedArr(4));
+// console.log(getUpdatedArr(202));
+// console.log(getUpdatedArr({name: 'Vasya'}));
+// console.log(getUpdatedArr());
+// console.log(getUpdatedArr(4));
 
+// task 3
+console.log('Задание 3')
+
+let diference = function() {
+    let count = 0;
+    let firstCall;
+    let interval;
+    return () => {
+        count++;
+        if (count == 1) {
+            firstCall = Date.now();
+            return 'Enabled';
+        }
+        let lastCall = Date.now();
+        interval =  Math.round((lastCall - firstCall) / 1000);
+        firstCall = lastCall;
+        return interval;
+      };
+    
+};
+
+let getTime = diference()
+
+console.log(getTime());
+// console.log(getTime());
+// console.log(getTime());
+
+
+
+// task 4
+console.log('Задание 4');
+
+const timer = (time) => { setInterval(function () {
+        seconds = time % 60;
+        minutes = Math.floor(time/60);
+        if (time <= 0) {
+            clearInterval(timer);
+            console.log("Time end");
+        } else { 
+            let strTimer = `${minutes}:${seconds}`;
+            if(minutes < 10) {
+                strTimer = `0${minutes}:${seconds}`;
+            }
+            if(seconds < 10) {
+                strTimer = `${minutes}:0${seconds}`;
+            }
+            if(seconds < 10 && minutes < 10) {
+                strTimer = `0${minutes}:0${seconds}`;
+            }
+            console.log(strTimer);
+        }
+        --time; 
+    }, 1000)
+}
+
+// timer(10);
